968048dfb34e11b95c906047291f5d596da9ad16
Maxim Krivoshein <max.kriv0s@gmail.com>
2024-11-04 21:14:45 +0300

    project complete

---
+++ src/layout/Main.jsx
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+import Movies from '../components/Movies';
+import Preloader from '../components/Preloader';
+import Search from '../components/Search';
+
+const APY_KEY = import.meta.env.VITE_APY_KEY;
+
+class Main extends Component {
+  state = {
+    movies: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    fetch(`https://omdbapi.com?apikey=${APY_KEY}&s=matrix`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  }
+
+  searchMovies = (str, type = 'all') => {
+    this.setState({ loading: true });
+    fetch(`https://omdbapi.com?apikey=${APY_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  };
+
+  render() {
+    const { movies, loading } = this.state;
+
+    return (
+      <main className='container content'>
+        <Search searchMovies={this.searchMovies} />
+        {loading ? <Preloader /> : <Movies movies={movies} />}
+      </main>
+    );
+  }
+}
+
+export default Main;
